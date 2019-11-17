307b6d59d1acb279e4ee3ed4ebf8bfe6e5129e6e
mloureiro93 <mariamadureiraloureiro@gmail.com>
2019-11-17 19:41:05 +0000

    celebrities part

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,48 +1,51 @@
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const logger = require('morgan');
-const path = require('path');
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const logger = require("morgan");
+const path = require("path");
 
 const app = express();
 
 // Express View engine setup
 app.use(
-  require('node-sass-middleware')({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
     sourceMap: true
   })
 );
 
 // Express View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.locals.title = 'Lab Mongoose Movies';
+app.locals.title = "Lab Mongoose Movies";
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
-const index = require('./routes/index');
-app.use('/', index);
+const index = require("./routes/index");
+app.use("/", index);
+
+const celebrities = require("./routes/celebrities");
+app.use("/celebrities", celebrities);
 
 // catch 404 and render a not-found.hbs template
 app.use((req, res, next) => {
   res.status(404);
-  res.render('not-found');
+  res.render("not-found");
 });
 
 app.use((error, req, res, next) => {
-  console.error('ERROR', req.method, req.path, error);
+  console.error("ERROR", req.method, req.path, error);
   res.status(500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
