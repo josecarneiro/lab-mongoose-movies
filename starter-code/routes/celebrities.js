a985b224daa1afab9ba065b1d3bab1a78b937a0f
another-dj <alexis_forestier@hotmail.com>
2019-11-18 00:23:43 +0000

    not finished

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -4,10 +4,72 @@ const Celebrity = require("../models/celebrity");
 
 // Handle GET request for website root
 router.get("/celebrities", (req, res, next) => {
-  Celebrity.find().then(celebrities => {
-    console.log(celebrities);
-    res.render("celebrities/index", { celebrities });
-  });
+  Celebrity.find()
+    .then(celebrities => {
+      console.log(celebrities);
+      res.render("celebrities/index", { celebrities });
+    })
+    .catch(error => {
+      next(error);
+      console.log(error);
+    });
+});
+
+router.get("/celebrities/new", (req, res, next) => {
+  res.render("celebrities/new");
+});
+
+router.get("/celebrities/:id", (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then(celebrity => {
+      console.log(celebrity);
+      res.render("celebrities/show", { celebrity });
+    })
+    .catch(error => {
+      next(error);
+      console.log(error);
+    });
+});
+
+/* router.post("/celebrities", (req, res, next) => {
+  new Celebrity({
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  })
+    .save()
+    .then(() => {
+      res.redirect("celebrities/");
+    })
+    .catch(error => {
+      console.log("Error creating new celebrity", error);
+      res.render("celebrities/new");
+    });
+}); */
+router.post("/celebrities", (req, res, next) => {
+  new Celebrity({
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  })
+    .save()
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch(error => {
+      next(error);
+      res.render("celebrities/new");
+    });
+});
+
+router.post("/celebrities/:id/delete", (req, res, next) => {
+  Celebrity.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 module.exports = router;
