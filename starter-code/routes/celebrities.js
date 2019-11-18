55d593279b41dfb57a61c58840f3e504e931c68b
another-dj <alexis_forestier@hotmail.com>
2019-11-18 01:28:41 +0000

    just done the editing celebs

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -72,4 +72,42 @@ router.post("/celebrities/:id/delete", (req, res, next) => {
     });
 });
 
+router.get("/celebrities/:id/edit", (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then( details => {
+      res.render("celebrities/edit" , { details });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+router.post("/celebrities/:id", (req, res, next) => {
+  Celebrity.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  })
+  .then( () => {
+    res.redirect("/celebrities");
+  })
+    .catch(error => {
+      next(error);
+    });
+});
+
+/* router.post("/:id", (req, res, next) => {
+  Celebrity.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  })
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch(error => {
+      console.log("Error trying to edit", error);
+    });
+}); */
+
 module.exports = router;
