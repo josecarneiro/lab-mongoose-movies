4d66c7a16912b633d7a967cae940f0e3bc8af4d5
Manuel Rocha <manueltemidorocha@gmail.com>
2020-02-14 18:01:05 +0000

    iteration 2 missing link

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Celebrity = require('./../models/celebrity');
 
 // Handle GET request for website root
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/celebrities', (req, res, next) => {
+  Celebrity.find().then(celebrities => res.render('celebrities/index', { celebrities }));
 });
 
 module.exports = router;
