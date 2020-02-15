f82b0cc2265b91f6205404dea61704d9a78f7808
André Ferreira <mrawesomeferreira@gmail.com>
2020-02-15 23:51:04 +0000

    Finished celebrities section

--- seed.js
+++ seed.js
@@ -4,3 +4,49 @@
 
 // You should be running this script once with `node seed.js` to add the records you need to the database,
 // not continuously, as we'll do with our express server
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const Celebrity = require('./models/celebrity');
+
+const data = [
+  {
+    name: 'Ed North',
+    occupation: 'actor',
+    catchPhrase: 'If you say so.'
+  },
+  {
+    name: 'Dave Chap',
+    occupation: 'comedian',
+    catchPhrase: 'WHAT?!'
+  },
+  {
+    name: 'Kung-Fu Kenny',
+    occupation: 'singer',
+    catchPhrase: 'Be humble.'
+  }
+];
+
+mongoose
+  .connect(MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connecting to MongoDB');
+    return Celebrity.create(data);
+  })
+  .then(() => {
+    console.log('List has been created');
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log('Disconnected from MongoDB');
+  })
+  .catch(error => {
+    console.log(error);
+  });
