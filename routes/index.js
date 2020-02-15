ca29727e9494f8ac5d958a71a3f89e4fbf2a92ae
Manuel Rocha <manueltemidorocha@gmail.com>
2020-02-15 09:46:14 +0000

    it3 solved

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,24 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/celebrities', (req, res, next) => {
-  Celebrity.find().then(celebrities => res.render('celebrities/index', { celebrities }));
+  Celebrity.find()
+    .then(celebrities => res.render('celebrities/index', { celebrities }))
+    .catch(error => {
+      console.log(error);
+      res.send('Error');
+    });
+});
+
+router.get('/celebrities/:id', (req, res, next) => {
+  const id = req.params.id;
+  Celebrity.findById(id)
+    .then(celebrity => {
+      return res.render('celebrities/show', celebrity);
+    })
+    .catch(error => {
+      console.log(error);
+      res.send('Error');
+    });
 });
 
 module.exports = router;
