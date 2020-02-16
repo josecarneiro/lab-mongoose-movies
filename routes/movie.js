f8752a40fe90048e36d7fa80ebe92a4b5239498c
Ric Az <ricardoazevedo96@gmail.com>
2020-02-16 19:00:31 +0000

    done

--- routes/movie.js
+++ routes/movie.js
@@ -13,13 +13,70 @@ router.get('/', (req, res, next) => {
       res.send('Error');
     });
 });
+router.post('/:id/delete', (req, res, next) => {
+  let id = req.params.id;
+  Movie.findByIdAndDelete(id)
+    .then(() => {
+      console.log('deleted');
+      res.redirect(`/movie`);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+router.get('/create', (req, res, next) => {
+  res.render('movie/create');
+});
+
+router.post('/create', (req, res, next) => {
+  const data = {
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot
+  };
+
+  Movie.create(data)
+    .then(() => {
+      res.redirect(`/movie`);
+    })
+    .catch(() => {
+      res.render('movie/create');
+    });
+});
+
+router.get('/:id/edit', (req, res, next) => {
+  let _id = req.params.id;
+  Movie.findById(_id)
+    .then(id => {
+      console.log(id);
+      res.render('movie/edit', { id });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+router.post('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const data = {
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot
+  };
+  Movie.findByIdAndUpdate(id, data, { runValidators: true })
+    .then(() => {
+      res.redirect(`/movie`);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
 
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
   Movie.findById(id)
     .then(data => {
       console.log(data);
-      res.render('/movie/smovie', { data });
+      res.render('movie/show', { data });
     })
     .catch(error => {
       console.log(error);
